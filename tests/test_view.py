44947605086b8fad2ae0a269089c034b55fa1abe
Max Hebditch <4867412+maxhebditch@users.noreply.github.com>
2024-09-28 14:20:49 +0100

    tests for view and new buttons

--- tests/test_view.py
+++ tests/test_view.py
@@ -9,7 +9,9 @@ from utils import note_data
 from epii.controller.controller import Controller
 from epii.model.model import Model
 from epii.notes.note import Note
-from epii.view.view import Button, Header, View, min_height, min_width
+from epii.view.view import Header, View, min_height, min_width
+from epii.view.buttons.nav_buttons import LeftNoteButton, RightNoteButton
+from epii.view.buttons.update_buttons import UpdateButton
 from epii.view_model.view_model import ViewModel
 
 
@@ -35,11 +37,13 @@ def test_init_ui(view):
     assert isinstance(view.centralWidget(), QWidget)
     assert isinstance(view.centralWidget().layout(), QVBoxLayout)
     assert isinstance(view.centralWidget().layout().itemAt(0).widget(), Header)
-    assert isinstance(view.centralWidget().layout().itemAt(1).widget(), Button)
-    assert isinstance(view.centralWidget().layout().itemAt(2).widget(), Button)
+    assert isinstance(view.centralWidget().layout().itemAt(1).widget(), UpdateButton)
+    assert isinstance(view.centralWidget().layout().itemAt(2).widget(), LeftNoteButton)
+    assert isinstance(view.centralWidget().layout().itemAt(3).widget(), RightNoteButton)
+    assert view.centralWidget().layout().count() == 4
 
 
-def test_header_init_ui():
+def test_header_init_ui(view):
     viewmodel = Mock(spec=ViewModel)
     view_data_return = "Note Content 0"
     viewmodel.get_data.return_value = view_data_return
@@ -47,18 +51,29 @@ def test_header_init_ui():
     assert header.text() == view_data_return
 
 
-def test_button_init_ui():
+def test_increment_note_button_init_ui(view):
     controller = Mock(spec=Controller)
-    button = Button(controller)
+    button = UpdateButton(controller)
+    assert button.text() == "Increment"
+
+
+def test_left_note_button_init_ui(view):
+    controller = Mock(spec=Controller)
+    button = LeftNoteButton(controller)
     assert button.text() == "Left"
 
 
-def test_header_update_text_on_button_press():
+def test_right_note_button_init_ui(view):
+    controller = Mock(spec=Controller)
+    button = RightNoteButton(controller)
+    assert button.text() == "Right"
+
+
+def test_header_update_text_on_emit(view):
     viewmodel = ViewModel(Model([Note(**note_data)]))
     controller = Controller(viewmodel)
 
     header = Header(viewmodel)
-    button = Button(controller)
 
     spy = QSignalSpy(viewmodel.data_changed)
 
@@ -66,7 +81,28 @@ def test_header_update_text_on_button_press():
     assert header.text() == f"{note_data['content']} 0"
     assert spy.count() == 0
 
-    QTest.mouseClick(button, Qt.LeftButton)
+    with patch("epii.view_model.ViewModel.get_data") as mock_get_data:
+        new_data = "New Note Content"
+        mock_get_data.return_value = new_data
+        viewmodel.data_changed.emit()
+
+        assert header.text() == new_data
+
+
+def test_header_update_text_on_button_press(view):
+    viewmodel = ViewModel(Model([Note(**note_data)]))
+    controller = Controller(viewmodel)
+
+    header = Header(viewmodel)
+    update_button = UpdateButton(controller)
+
+    spy = QSignalSpy(viewmodel.data_changed)
+
+    assert header.text() == viewmodel.get_data()
+    assert header.text() == f"{note_data['content']} 0"
+    assert spy.count() == 0
+
+    QTest.mouseClick(update_button, Qt.LeftButton)
     assert header.text() == viewmodel.get_data()
     assert header.text() == f"{note_data['content']} 1"
     assert spy.count() == 1
